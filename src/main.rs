0df0d62876fc633cedd537ce947d1ea38325c45f
Brian Todoroff <brian@todoroff.com>
2024-09-02 23:01:23 -0700

    Implement EmMemory type. Bring Enum varients into scope for SfrAddr and ControlMsg

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,62 @@ mod em8051 {
     }
 
     #[derive(Debug)]
+    pub struct EmMemory {
+	mem: Vec<u8>,
+    }
+
+    impl EmMemory {
+	pub fn new(s: usize) -> EmMemory {
+	    EmMemory {
+		mem: Vec::with_capacity(s),
+	    }
+	}
+    }
+    impl Index<u16> for EmMemory {
+	type Output = u8;
+	fn index(&self, addr: u16) -> &Self::Output {
+	    if addr as usize > self.mem.capacity() {
+		&self.mem[addr as usize % self.mem.capacity()]
+	    }
+	    else {		
+		&self.mem[addr as usize]
+	    }
+	}
+    }
+    impl IndexMut<u16> for EmMemory {
+	fn index_mut(&mut self, addr: u16) -> &mut Self::Output {
+	    if addr as usize > self.mem.capacity() {
+		let cap = self.mem.capacity();
+		&mut self.mem[addr as usize & cap]
+	    } else {
+		&mut self.mem[addr as usize]
+	    }
+	}
+    }
+    impl Index<u8> for EmMemory {
+	type Output = u8;
+	fn index(&self, addr: u8) -> &Self::Output {
+	    if addr as usize > self.mem.capacity() {
+		&self.mem[addr as usize % self.mem.capacity()]
+	    }
+	    else {		
+		&self.mem[addr as usize]
+	    }
+	}
+    }
+    impl IndexMut<u8> for EmMemory {
+	fn index_mut(&mut self, addr: u8) -> &mut Self::Output {
+	    if addr as usize > self.mem.capacity() {
+		let cap = self.mem.capacity();
+		&mut self.mem[addr as usize & cap]
+	    } else {
+		&mut self.mem[addr as usize]
+	    }
+	}
+    }
+
+    
+    #[derive(Debug)]
     struct InternalMemory([u8;256]);
     impl Index<SfrAddr> for InternalMemory {
         type Output = u8;
@@ -81,7 +137,7 @@ mod em8051 {
     pub struct State {
         pub run: bool,
         pub pc: u16,
-        pub cpu_regfile: [u8; 16],
+        pub cpu_regfile: EmMemory,
         pub pgm_memory: [u8; 16*1024],
         data_memory: [u8; 16*1024],
         int_memory: InternalMemory, // [u8; 256],
@@ -120,7 +176,7 @@ mod em8051 {
         let state = Arc::new(Mutex::new(State {
             run: false,
             pc: 0,
-            cpu_regfile: [0; 16],
+            cpu_regfile: EmMemory::new(16),
             pgm_memory: [0; 16*1024],
             data_memory: [0; 16*1024],
             int_memory: InternalMemory([0; 256]),
@@ -136,25 +192,26 @@ mod em8051 {
                 match ctrlrx.try_recv() {
                     Err(_) => { break; },
                     Ok(ctl_msg) => {
+			    use ControlMsg::*;
                             match ctl_msg {
-                                ControlMsg::Halt => {
+                                Halt => {
                                     let mut state = state_mutex.lock().unwrap();
                                     state.run = false;
                                 }
-                                ControlMsg::Reset => {
+                                Reset => {
                                     let mut state = state_mutex.lock().unwrap();
                                     reset(&mut state);
                                 },
-                                ControlMsg::PowerOn => {
+                                PowerOn => {
                                     let mut state = state_mutex.lock().unwrap();
                                     reset(&mut state);
                                     state.run = true;
                                 },
-                                ControlMsg::Continue => {
+                                Continue => {
                                     let mut state = state_mutex.lock().unwrap();
                                     state.run = true;
                                 },
-                                ControlMsg::ContinueFor(clocks) => {
+                                ContinueFor(clocks) => {
                                     let mut state = state_mutex.lock().unwrap();
                                     state.break_clock_cycle = state.clock_cycle+clocks;
                                     state.run = true;
@@ -180,51 +237,51 @@ mod em8051 {
 
     fn tick (state: &mut State) {
         let instruction:u8 = state.pgm_memory[state.pc as usize];
-
+	use SfrAddr::*;
         if instruction == 0x00 { // NOP
 	    state.pc+=1;
 	} else
 	if (instruction & 0b00011111)==0b00010001 { // ACALL addr11
             state.pc+=2;
-            let sp_value = state.int_memory[SfrAddr::SP];
-            state.int_memory[SfrAddr::SP] += 1;
+            let sp_value = state.int_memory[SP];
+            state.int_memory[SP] += 1;
             state.int_memory[sp_value+1]=(state.pc & 0x00ff) as u8;
-            state.int_memory[SfrAddr::SP] += 1;
+            state.int_memory[SP] += 1;
             state.int_memory[sp_value+2]=(state.pc >> 8) as u8;
             state.pc = ((instruction & 0b11100000) as u16) << 8;
             state.pc += state.pgm_memory[(state.pc-1) as usize] as u16;
         } else
             if (instruction & 0b11111000)==0b00101000 { // ADD A,Rn
-		state.int_memory[SfrAddr::ACC] += state.cpu_regfile[(instruction & 0b00000111) as usize] as u8;
+		state.int_memory[ACC] += state.cpu_regfile[instruction & 0b00000111];
 		state.pc+=1;
             } else
             if instruction == 0b00100101 { // ADD A,direct
-		state.int_memory[SfrAddr::ACC] += state.int_memory[state.int_memory[state.pc]];
+		state.int_memory[ACC] += state.int_memory[state.int_memory[state.pc]];
 		state.pc += 2;
             } else
 	    if (instruction & 0b11111110) == 0b00100110 { // ADD A,@Ri
-		state.int_memory[SfrAddr::ACC] += state.cpu_regfile[(instruction & 0x00000001) as usize] as u8;
+		state.int_memory[ACC] += state.cpu_regfile[instruction & 0x00000001];
 		state.pc += 1;
 	    } else
 	    if instruction == 0b00100100 { // ADD A,#data
 		state.pc += 1;
-		state.int_memory[SfrAddr::ACC] += state.pgm_memory[state.pc as usize];
+		state.int_memory[ACC] += state.pgm_memory[state.pc as usize];
 	    } else
 	    if (instruction & 0b11111000) == 0b00111000 { // ADDC A,RN
-		state.int_memory[SfrAddr::ACC] += state.cpu_regfile[(instruction & 0b00000111) as usize] as u8 + state.carry();
+		state.int_memory[ACC] += state.cpu_regfile[instruction & 0b00000111] as u8 + state.carry();
 		state.pc+=1;
             } else
             if instruction == 0b00110101 { // ADDC A,direct
-		state.int_memory[SfrAddr::ACC] += state.int_memory[state.int_memory[state.pc]] + state.carry();
+		state.int_memory[ACC] += state.int_memory[state.int_memory[state.pc]] + state.carry();
 		state.pc += 2;
             } else
 	    if (instruction & 0b11111110) == 0b00110110 { // ADDC A,@Ri
-		state.int_memory[SfrAddr::ACC] += state.cpu_regfile[(instruction & 0x00000001) as usize] as u8 + state.carry();
+		state.int_memory[ACC] += state.cpu_regfile[instruction & 0x00000001] as u8 + state.carry();
 		state.pc += 1;
 	    } else
 	    if instruction == 0b00110100 { // ADDC A,#data
 		state.pc += 1;
-		state.int_memory[SfrAddr::ACC] += state.pgm_memory[state.pc as usize]+state.carry();
+		state.int_memory[ACC] += state.pgm_memory[state.pc as usize]+state.carry();
 		state.pc += 1;
 	    } else 
 	    if (instruction & 0b00011111) == 0b00000001 { // AJMP addr11
@@ -233,26 +290,26 @@ mod em8051 {
 		state.pc = (state.pc & 0x02ff) + addr;
 	    } else
 	    if (instruction & 0b11111000) == 0b01011000 { // ANL A,Rn
-		state.int_memory[SfrAddr::ACC] = state.int_memory[SfrAddr::ACC] & state.cpu_regfile[(instruction & 0x07) as usize];
+		state.int_memory[ACC] = state.int_memory[ACC] & state.cpu_regfile[instruction & 0x07];
 		state.pc += 1;
 	    } else
 	    if instruction == 0b01010101 { // ANL A,direct
 		state.pc += 1;
-		state.int_memory[SfrAddr::ACC] = state.int_memory[SfrAddr::ACC] & state.pgm_memory[state.pc as usize];
+		state.int_memory[ACC] = state.int_memory[ACC] & state.pgm_memory[state.pc as usize];
 		state.pc += 1;
 	    } else
 	    if (instruction & 0b11111110) == 0b01010110 { // ANL A,@Ri
-		state.int_memory[SfrAddr::ACC] = state.int_memory[SfrAddr::ACC] & state.cpu_regfile[(instruction & 0x00000001) as usize] as u8;
+		state.int_memory[ACC] = state.int_memory[ACC] & state.cpu_regfile[instruction & 0x00000001];
 		state.pc+=1;
 	    } else
 	    if instruction == 0b01010100 { // ANL A,#data
 		state.pc += 1;
-		state.int_memory[SfrAddr::ACC] = state.pgm_memory[state.pc as usize]+state.carry();
+		state.int_memory[ACC] = state.pgm_memory[state.pc as usize]+state.carry();
 		state.pc += 1;
 	    } else
 	    if instruction == 0b01010010 { // ANL direct,A
 		state.pc += 1;
-		state.int_memory[SfrAddr::ACC] = state.pgm_memory[state.pc as usize] & state.int_memory[SfrAddr::ACC];
+		state.int_memory[ACC] = state.pgm_memory[state.pc as usize] & state.int_memory[ACC];
 		state.pc += 1;
 	    } else
         {};
